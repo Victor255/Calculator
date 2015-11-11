acf53b9ff8bbf03d7fcc902bd3f699c2bbeefd06
Victor255 <tagbar95@gmail.com>
2015-11-11 15:23:08 -0600

    project with documentation of the functions

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ def menu_two():
     select_menu()
 
 def menu():
+    """SHOWS THE INSTRUCTIONS"""
     os.system("clear")
     print "_____________________________________________"
     print "|Calculator!                                 |"
@@ -17,6 +18,7 @@ def menu():
     print "|____________________________________________|"
 
 def optis():
+    """SHOWS THE RESPONSE ON THE SCREEN shows"""
     option_one = Enter_number()
     option_two = Enter_number()
     print "\nThe answer is:"
@@ -24,56 +26,71 @@ def optis():
 
 def Enter_number():
     while True:
-        answer = raw_input("\nEnter a option please: ")
+        answer = raw_input("\nEnter a Number please: ")
         try:
             answer = int(answer)
             return answer
             break
         except ValueError:
             print "\nTry again."
+
 def Incognit():
+    """ASK IF THE USER WANTS TO MAKE OTHER OPERATION """
     selected = True
     while selected == True:
-        selection = raw_input("Do you want make other operation? yes or no: ")
+        selection = raw_input("Do you want to continue to use the calculator? yes or no: ")
         if selection == "y" or selection == "yes":
             menu_two()
         elif selection == "n" or selection == "no":
             sys.exit(1)
         else:
-            print "Insert yes or no."
+            print "Insert only yes or no."
 
 def select_menu():
+    """MENU OF OPTIONS"""
     opti = Enter_number()
     validate = True
     while validate == True:
         if opti == 1:
+            print "You are Doing a sum"
             print str(sum(optis()))
             Incognit()
         elif opti == 2:
+            print "You are Doing a Subtraction"
             print str(subtraction(optis()))
             Incognit()
         elif opti == 3:
+            print "You are Doing a Multiplication"
             print str(multiplication(optis()))
             Incognit()
         elif opti == 4:
+            print "You are Doing a Division"
             print str(division(optis()))
             Incognit()
         elif opti == 5:
-            print "Good bye"
+            print "Good bye!!"
             sys.exit(1)
         else:
             menu_two()
 
+#Test1
 def sum(numbers):
+    """THIS FUNCTION WILL RETURN THE SUM"""
     return numbers[0] + numbers[1]
 
+#Test2
 def subtraction(numbers):
+    """THIS FUNCTION WILL RETURN THE SUBTRACTION"""
     return numbers[0] - numbers[1]
 
+#Test3
 def multiplication(numbers):
+    """THIS FUNCTION WILL RETURN THE MULTIPLICATION"""
     return numbers[0] * numbers[1]
 
+#Test4
 def division(numbers):
+    """THIS FUNCTION WILL RETURN THE DIVISION"""
     return numbers[0] / numbers[1]
 
 if __name__ == '__main__':
