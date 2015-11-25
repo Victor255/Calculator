5037bfc63f25ac41a882ce56e63cea3f4f57523a
Victor255 <tagbar95@gmail.com>
2015-11-25 12:25:32 -0600

    subtraction and multiplication test

--- unitest.py
+++ unitest.py
@@ -8,9 +8,11 @@ class Test_aplication(unittest.TestCase):
 
 	def test_subtraction(self):
 		self.assertEqual(application.subtraction([5, 3]), 2)
+		self.assertEqual(application.subtraction([1, 2]), -1)
 
 	def test_multiplication(self):
 		self.assertEqual(application.multiplication([5, 5]), 25)
+		self.assertEqual(application.multiplication([7, 0]),  0)
 
 	def test_division(self):
 		self.assertEqual(application.division([9, 3]), 3)
